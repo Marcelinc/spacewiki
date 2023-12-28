1cb54ec9c6cff461ba846cd5c6b7fa1351fbc9da
Marcelinc <marcin.c40@wp.pl>
2023-12-28 13:32:20 +0100

    Added modal to display images from slider

---
+++ src/components/FullScreenImg.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    objectName: {
+        type: String,
+        required: true,
+        defaultValue: "Galaxy"
+    },
+    imgUrl: {
+            type: String,
+            required: true,
+    }
+})
+
+
+
+</script>
+
+<template>
+    <div id="fullSizeImg-container">
+        <div id="fullSizeImg-content">
+            <img v-bind:src="props.imgUrl" :alt="props.objectName" />
+        </div>
+        <span id="close-button" @click="$emit('closeImage')">X</span>
+    </div>
+</template>
+
+<style scoped>
+#fullSizeImg-container{
+    background-color: rgba(51, 52, 53, 0.767);
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    z-index: 3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#close-button{
+    right: 0;
+    top: 0;
+    position: fixed;
+    margin: 2em;
+    font-weight: 900;
+    font-size: 30px;
+    color: aqua;
+}
+#close-button:hover{
+    cursor: pointer;
+    opacity: .8;
+}
+img{
+    width: 80vw;
+    height: 70vh;
+    object-fit: contain;
+}
+</style>
